0d2f34bb6624f06e6be0c091e1e406b0fcd9a38c
rodri-oliveira <rodrigoleao.ci@gmail.com>
2022-01-02 23:39:25 -0300

    hacker rank exercise starCase

---
+++ stairCase.js
@@ -0,0 +1,17 @@
+function stairCase(n){
+    let height = n
+    for(let i = 0; i < height; i++){
+        let row = ""
+
+        for(let j = 0; j < height; j++){
+            if(i + j > height - 2){
+                row += "#"
+            }else {
+                row += " "
+            }
+        }
+        console.log(row)
+    }
+}
+
+stairCase(4)
